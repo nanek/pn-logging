fd65f63f84648f5d68e48a9c25cf78828c71e7a9
Daiwei <daiweilu123@gmail.com>
2016-01-12 18:24:52 -0500

    Add tests

--- test/index.js
+++ test/index.js
@@ -23,7 +23,7 @@ describe('index', function () {
   describe('Log constructor', function () {
 
     it('should return a Log instance', function () {
-      var log = new index.Log({ transports: [] });
+      var log = new index.Log({ transports: [], sentry: {} });
 
       expect(log).to.be.an.instanceOf(index.Log);
     });
@@ -36,49 +36,49 @@ describe('index', function () {
     });
 
     it('should have a debug method', function () {
-      var log = new index.Log({ transports: [] });
+      var log = new index.Log({ transports: [], sentry: {} });
 
       expect(log).to.respondTo('debug');
     });
 
     it('should have a info method', function () {
-      var log = new index.Log({ transports: [] });
+      var log = new index.Log({ transports: [], sentry: {} });
 
       expect(log).to.respondTo('info');
     });
 
     it('should have a notice method', function () {
-      var log = new index.Log({ transports: [] });
+      var log = new index.Log({ transports: [], sentry: {} });
 
       expect(log).to.respondTo('notice');
     });
 
     it('should have a warning method', function () {
-      var log = new index.Log({ transports: [] });
+      var log = new index.Log({ transports: [], sentry: {} });
 
       expect(log).to.respondTo('warning');
     });
 
     it('should have a error method', function () {
-      var log = new index.Log({ transports: [] });
+      var log = new index.Log({ transports: [], sentry: {} });
 
       expect(log).to.respondTo('error');
     });
 
     it('should have a crit method', function () {
-      var log = new index.Log({ transports: [] });
+      var log = new index.Log({ transports: [], sentry: {} });
 
       expect(log).to.respondTo('crit');
     });
 
     it('should have a alert method', function () {
-      var log = new index.Log({ transports: [] });
+      var log = new index.Log({ transports: [], sentry: {} });
 
       expect(log).to.respondTo('alert');
     });
 
     it('should have a emerg method', function () {
-      var log = new index.Log({ transports: [] });
+      var log = new index.Log({ transports: [], sentry: {} });
 
       expect(log).to.respondTo('emerg');
     });
@@ -104,7 +104,8 @@ describe('index', function () {
             level: 'debug',
             __log: spy
           }
-        }]
+        }],
+        sentry: {}
       });
     });
 
@@ -146,18 +147,98 @@ describe('index', function () {
   describe('middleware', function () {
 
     it('should have middleware on the instance', function () {
-      var log = new index.Log({ transports: [] });
+      var log = new index.Log({ transports: [], sentry: {} });
 
       expect(log).to.have.property('middleware');
       expect(log.middleware).to.be.a('function');
     });
 
     it('should produce a middleware function', function () {
-      var log = new index.Log({ transports: [] });
+      var log = new index.Log({ transports: [], sentry: {} });
       var middleware = log.middleware();
 
       expect(middleware).to.be.a('function');
       expect(middleware).to.have.length(3);
+    });
+
+  });
+
+  describe('_getSentryMeta', function () {
+
+    it('should return null if meta is not defined', function () {
+      var result = index._getSentryMeta();
+
+      expect(result).to.be.null;
+    });
+
+    it('should add tags property', function () {
+      var input = {
+        tags: {
+          key1: 'value1',
+          key2: 'value2'
+        }
+      };
+
+      var result = index._getSentryMeta(input);
+
+      expect(result).to.have.property('tags');
+      expect(result.tags).to.eql({
+        key1: 'value1',
+        key2: 'value2'
+      });
+    });
+
+    it('should add fingerprint property', function () {
+      var input = {
+        fingerprint: 'fingerprintValue'
+      };
+
+      var result = index._getSentryMeta(input);
+
+      expect(result).to.have.property('fingerprint', 'fingerprintValue');
+    });
+
+    it('should add fingerprint property', function () {
+      var input = {
+        level: 'error'
+      };
+
+      var result = index._getSentryMeta(input);
+
+      expect(result).to.have.property('level', 'error');
+    });
+
+    it('should exclude tags, fingerprint, and level in extra', function () {
+      var input = {
+        level: 'error',
+        tags: 'tagsValue',
+        fingerprint: 'fingerprintValue',
+        key1: 'value1',
+        key2: 'value2'
+      };
+
+      var result = index._getSentryMeta(input);
+
+      expect(result).to.have.property('extra');
+      expect(result.extra).to.not.have.property('level');
+      expect(result.extra).to.not.have.property('tags');
+      expect(result.extra).to.not.have.property('fingerprint');
+    });
+
+    it('should include other property in extra', function () {
+      var input = {
+        level: 'error',
+        tags: 'tagsValue',
+        fingerprint: 'fingerprintValue',
+        key1: 'value1',
+        key2: 'value2'
+      };
+
+      var result = index._getSentryMeta(input);
+
+      expect(result).to.have.property('extra');
+      expect(result.extra).to.have.property('key1', 'value1');
+      expect(result.extra).to.have.property('key2', 'value2');
     });
 
   });
