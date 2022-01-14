640bbe1822f0ad1150c97a227363721636181b4f
Michael Kalish <michael@spanishdict.com>
2022-01-14 13:37:54 -0500

    Chore: update repo for linting testing TS support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,5 +1,10 @@
 module.exports = {
-  extends: ['eslint:recommended'],
+  // extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  // The next three lines can be removed and and the above uncommented, but
+  // there are currently 113 problems that need resolving
+  plugins: ["@typescript-eslint"],
+  extends: ["eslint:recommended"],
+  parser: "@typescript-eslint/parser",
   env: {
     node: true,
     es6: true,
