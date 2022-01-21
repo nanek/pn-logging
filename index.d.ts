e2b737fe3fe5af057bf4ad7dd33dc07b00b0aa21
Michael Kalish <michael@spanishdict.com>
2022-01-21 07:08:54 -0500

    Chore: create ambient type declarations (#38)

---
+++ index.d.ts
@@ -0,0 +1,37 @@
+declare module "pn-logging" {
+  type sysLogLevels =
+    | "emerg"
+    | "alert"
+    | "crit"
+    | "error"
+    | "warning"
+    | "notice"
+    | "info"
+    | "debug";
+
+  type LoggingParameters = Parameters<
+    (
+      message: string,
+      meta: object,
+      err?: unknown,
+      req?: Express.Request,
+      res?: Express.Request
+    ) => void
+  >;
+
+  type ILog = {
+    [Action in sysLogLevels]: (...args: LoggingParameters) => void;
+  };
+
+  export class Log implements ILog {
+    constructor(options: unknown);
+    emerg(...args: LoggingParameters): void;
+    alert(...args: LoggingParameters): void;
+    crit(...args: LoggingParameters): void;
+    error(...args: LoggingParameters): void;
+    warning(...args: LoggingParameters): void;
+    notice(...args: LoggingParameters): void;
+    info(...args: LoggingParameters): void;
+    debug(...args: LoggingParameters): void;
+  }
+}
